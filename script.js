d92399cc9d825689c2b93aa0c2d6fb9fe8fd040a
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2021-09-13 16:44:42 -0300

    selecting the tiles and looping to attach events to them

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-const gameBoard = {
-  gameBoard: []
-};
-
-const players = {
-  playerX: 'X',
-  playerO: 'O'
-};
-
-const gameFlow = {
+(function  () {
+  // CACHE DOM
+  const tiles = Array.from(document.querySelectorAll('.tile'));
+  console.log(tiles)
   
-};
+  // SETTING THE BOARD
+  const gameBoard = {
+    gameBoard: ['','','',
+                '','','',
+                '','','']
+  };
+
+  // ADDING EVENT TO THE TILES
+  tiles.forEach( (tile, index) => {
+    tile.addEventListener('click', () => userAction(tile, index));
+});
+
+  
+  
+  const players = {
+    currentPlayer: 'X'
+  };
+  
+
+
+
+  // const gameFlow = {
+  //   createPlayer: (name, icon) =>{
+  //     return { name, icon };
+  // }};
+
+  // gameFlow.createPlayer('Adrian', 'X')
+
+})();
+
+
