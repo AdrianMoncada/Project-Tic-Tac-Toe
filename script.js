36ed03918566bf4401e21fe9538d3bd5ee4e9ed7
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2021-09-13 17:57:24 -0300

    displaying the player X clicks into the board

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 (function  () {
   // CACHE DOM
   const tiles = Array.from(document.querySelectorAll('.tile'));
-  console.log(tiles)
   
   // SETTING THE BOARD
-  const gameBoard = {
-    gameBoard: ['','','',
-                '','','',
-                '','','']
-  };
+    let board = ['','','',
+                  '','','',
+                  '','','']
+console.log(board)  
+
+ const currentPlayer = 'X'
+ let isGameActive = true;
 
   // ADDING EVENT TO THE TILES
   tiles.forEach( (tile, index) => {
     tile.addEventListener('click', () => userAction(tile, index));
 });
 
+  // SETTING THE USER ACTION WHEN TILES ARE CLICKED
+  const userAction = (tile, index) => {
+    if(isValidAction(tile) && isGameActive) {
+      tile.innerText = currentPlayer;
+      tile.classList.add(`player${currentPlayer}`);
+      updateBoard(index);
+
+    }
+  }
+
+  const isValidAction = (tile) => {
+    if (tile.innerText === 'X' || tile.innerText === 'O'){
+        return false;
+    }
+
+    return true;
+};
+
+const updateBoard =  (index) => {
+  board[index] = currentPlayer;
+  console.log(board)
+}
+
   
   
-  const players = {
-    currentPlayer: 'X'
-  };
+  
   
 
 
