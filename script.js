f0cfa489eda913e1d43eddfd2049da9b18eca988
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2021-09-13 22:24:46 -0300

    adding draw function

--- script.js
+++ script.js
@@ -3,14 +3,21 @@
   const tiles = Array.from(document.querySelectorAll('.tile'));
   
   // SETTING THE BOARD
-    let board = ['','','',
-                  '','','',
-                  '','','']
-console.log(board)  
+  let board = ['', '', '', '', '', '', '', '', ''];
+
 
  let currentPlayer = 'X'
- console.log(currentPlayer)
  let isGameActive = true;
+ const winningConditions =[
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
   // ADDING EVENT TO THE TILES
   tiles.forEach( (tile, index) => {
@@ -23,11 +30,13 @@ console.log(board)
       tile.innerText = currentPlayer;
       tile.classList.add(`player${currentPlayer}`);
       updateBoard(index);
+      handleResultValidation()
       changePlayer();
       console.log(currentPlayer)
     }
   }
 
+  // Checking if tiles are occupy or not
   const isValidAction = (tile) => {
     if (tile.innerText === 'X' || tile.innerText === 'O'){
         return false;
@@ -36,26 +45,34 @@ console.log(board)
     return true;
 };
 
+  // UPDATING THE STRING BOARD WITH THE CORRESPONDENT CURRENT PLAYER
 const updateBoard =  (index) => {
   board[index] = currentPlayer;
   console.log(board)
 }
 
+  // CHANGING PLAYERS
 const changePlayer = () => {
- if (currentPlayer === 'X') {
-  currentPlayer = 'O'
- } else {
-  currentPlayer = 'X'
- }
+currentPlayer === 'X'?currentPlayer = 'O':currentPlayer = 'X'
 }
-      
 
-  // const gameFlow = {
-  //   createPlayer: (name, icon) =>{
-  //     return { name, icon };
-  // }};
+function handleResultValidation() {
+  let roundWon = false;
+  for (let i = 0; i <= 7; i++) {
+      const winCondition = winningConditions[i];
+      const a = board[winCondition[0]];
+      const b = board[winCondition[1]];
+      const c = board[winCondition[2]];
+      if (a === '' || b === '' || c === '') {
+          continue;
+      }
+      if (a === b && b === c) {
+          roundWon = true;
+          console.log("there's a winner!")
+          break;
+      }
+  }};
 
-  // gameFlow.createPlayer('Adrian', 'X')
 
 })();
 
